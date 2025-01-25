fbc50b12b8a018ca254b056cf5e363ff751c2620
Quentin <112642301+QuentinBAUDRY@users.noreply.github.com>
2025-01-25 20:58:07 +0100

    Update Experience.tsx

--- src/components/sections/index/Experience.tsx
+++ src/components/sections/index/Experience.tsx
@@ -29,8 +29,8 @@ export default function Experience() {
                         media={[
                             "https://r2.e-z.host/4ed8b442-31c9-4738-a919-7ff8dee725df/vrv2o4g9.webp",
                             "https://r2.e-z.host/4ed8b442-31c9-4738-a919-7ff8dee725df/pu1z5f45.webp",
-                            "https://i.e-z.gg/h24u9sf7.webp",
-                            "https://i.e-z.gg/x275xpf2.webp",
+                            "https://r2.e-z.host/4ed8b442-31c9-4738-a919-7ff8dee725df/h24u9sf7.webp",
+                            "https://r2.e-z.host/4ed8b442-31c9-4738-a919-7ff8dee725df/x275xpf2.webp",
                         ]}
                         myRole="Founder"
                         timeline="July 2024 - Present"
