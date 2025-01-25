ad483d4cb40499e696f336529bce664348e86f2c
Quentin <112642301+QuentinBAUDRY@users.noreply.github.com>
2025-01-25 21:16:11 +0100

    Update _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -10,7 +10,7 @@ export default function App({ Component, pageProps }: AppProps) {
     <>
       <NextSeo
         title={"azurich.fr"}
-        description={"Aiden's (aka SKRRRTT) personal site"}
+        description={"Azurich personal site"}
         canonical={`https://azurich.fr${router.asPath.split("?")[0] === "/" ? "" : router.asPath.split("?")[0]}`}
         themeColor={"#2563eb"}
         openGraph={{
@@ -19,7 +19,7 @@ export default function App({ Component, pageProps }: AppProps) {
           description: "azurich personal site",
           images: [
             {
-              url: "https://cdn.discordapp.com/attachments/1032647397328830495/1032648354909397053/unknown.png?ex=67943ff9&is=6792ee79&hm=588014cae26e22c2bb4441f78f1a21a0abd500e52d5e091c68002f6415a6912c&",
+              url: "https://r2.e-z.host/4ed8b442-31c9-4738-a919-7ff8dee725df/7tgqsqub.png",
               alt: "kurt",
             },
           ],
