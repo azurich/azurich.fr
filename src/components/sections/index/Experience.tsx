329f7b43b3ce1799baf3ea20883d96ab46095304
Quentin <112642301+QuentinBAUDRY@users.noreply.github.com>
2025-01-25 19:25:05 +0100

    Update Experience.tsx

--- src/components/sections/index/Experience.tsx
+++ src/components/sections/index/Experience.tsx
@@ -24,13 +24,13 @@ export default function Experience() {
                         fullDescription={[
                             "Season World is a meticulously curated modpack designed to provide a diverse and immersive gaming experience. Each mod included in this pack has been selected to enhance various aspects of the game, from space exploration to expansive landscapes, improved travel systems, and practical survival tools. In addition to the mods, we have integrated resource packs and shaders to visually enhance your world.",
                         ]}
-                        cardImage="https://cdn.discordapp.com/attachments/931502258229751829/1332388281740038225/giqhlh0p.png?ex=679512c8&is=6793c148&hm=94a7c785c72e63aea4eec9e54262fd91d02b6a3342885508c73710e1f19118b9&"
+                        cardImage="https://drive.google.com/file/d/1FFIWvuYPBtPFbZODZzZDat_Ba_-uHjtp/view?usp=drive_link"
                         cardDescription="Season World is a meticulously curated modpack designed to provide a diverse and immersive gaming experience. Each mod included in this pack has been selected to enhance various aspects of the game, from space exploration to expansive landscapes, improved travel systems, and practical survival tools. In addition to the mods, we have integrated resource packs and shaders to visually enhance your world."
                         media={[
-                            "https://cdn.discordapp.com/attachments/931502258229751829/1332388436862042280/0da4a2b32941ae3b9e6b79da2cd7bd55acae2f42.png?ex=679512ed&is=6793c16d&hm=af5588057b94b8111222d8c9937c309575622d5db6433a0d5f41072c522132e1&",
-                            "https://cdn.discordapp.com/attachments/931502258229751829/1332388437793443891/b58d072cb143efab0bf418647e0728a5f5104633.png?ex=679512ee&is=6793c16e&hm=c7a8786878cc06160043bd925bcf0ae3b9d8cbd0a789652bc962b078dec47618&",
-                            "https://cdn.discordapp.com/attachments/931502258229751829/1332388438783299634/b513dc2bdceb0882347b3bd02c7b47c436efedd4.png?ex=679512ee&is=6793c16e&hm=bd5e1508f7c0f69b2124ea4708f3bcf4c17fa9389560964d07f05ecb5f130fc0&",
-                            "https://cdn.discordapp.com/attachments/931502258229751829/1332388439710109727/f6b19b51066def1548efd7176d4d3d16e64e6fe1.png?ex=679512ee&is=6793c16e&hm=429d6845a0c8313a3d9ae8f0f2ffd209b9e413b4b0e84230b2ed48a1d3e31634&",
+                            "https://drive.google.com/file/d/1ni-BZbe2FxRtNFHJC3a49rSv66yZQ6c0/view?usp=drive_link",
+                            "https://drive.google.com/file/d/1xoWWTYQAqVaQXqmGT9y7aiGUMp0R3KqR/view?usp=drive_link",
+                            "https://drive.google.com/file/d/1PakJ0KSkpjIyQArxmMPCxvNNc3dBdLZi/view?usp=drive_link",
+                            "https://drive.google.com/file/d/1nOPbvTEWiHyrTkuihlXt7OCqCDYHANAE/view?usp=drive_link",
                         ]}
                         myRole="Founder"
                         timeline="July 2024 - Present"
