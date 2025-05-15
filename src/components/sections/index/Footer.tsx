28b3613847809d633744cd6d90aa0f5c16bf1820
Quentin <112642301+azurich@users.noreply.github.com>
2025-05-15 09:35:19 +0200

    Update Footer.tsx

--- src/components/sections/index/Footer.tsx
+++ src/components/sections/index/Footer.tsx
@@ -6,7 +6,7 @@ export default function Footer() {
             <footer className="max-w-4xl w-full flex flex-col mx-auto pt-4 pb-2">
                 <Divider />
                 <p className="text-center font-semibold text-base pt-2">
-                    Made by aiden.gg 👑
+                    Made by azurich with 💖
                 </p>
                 <p className="text-center font-medium brightness-75 text-base">
                     Last updated: May 15th, 2025
