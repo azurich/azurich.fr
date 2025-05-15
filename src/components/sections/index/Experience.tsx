2bf96efbb7b89aebf71d8941d5e9f77cc1b38e46
Quentin <112642301+azurich@users.noreply.github.com>
2025-05-15 09:02:19 +0200

    Update Experience.tsx

--- src/components/sections/index/Experience.tsx
+++ src/components/sections/index/Experience.tsx
@@ -20,12 +20,12 @@ export default function Experience() {
                 <ul className={`flex flex-col pt-6 pb-1 gap-4 overflow-hidden`}>
                     <ExperienceCard
                         url="https://modrinth.com/modpack/seasonworld"
-                        title="Season World Modpack"
+                        title="Mods Manager"
                         fullDescription={[
-                            "Season World is a meticulously curated modpack designed to provide a diverse and immersive gaming experience. Each mod included in this pack has been selected to enhance various aspects of the game, from space exploration to expansive landscapes, improved travel systems, and practical survival tools. In addition to the mods, we have integrated resource packs and shaders to visually enhance your world.",
+                            "Mods Manager is a lightweight and user-friendly application designed to simplify the management of Minecraft modpacks, particularly for CurseForge-based instances. It allows users to easily switch between modpack instances, remove outdated mods, and automatically install or update required mods with one click.",
                         ]}
                         cardImage="https://r2.e-z.host/4ed8b442-31c9-4738-a919-7ff8dee725df/6mtmembk.webp"
-                        cardDescription="Season World is a meticulously curated modpack designed to provide a diverse and immersive gaming experience. Each mod included in this pack has been selected to enhance various aspects of the game, from space exploration to expansive landscapes, improved travel systems, and practical survival tools. In addition to the mods, we have integrated resource packs and shaders to visually enhance your world."
+                        cardDescription="Mods Manager is a lightweight and user-friendly application designed to simplify the management of Minecraft modpacks, particularly for CurseForge-based instances. It allows users to easily switch between modpack instances, remove outdated mods, and automatically install or update required mods with one click. Whether you're a server administrator or a casual modded Minecraft player, Mods Manager saves time and eliminates repetitive tasks by automating your mod installation and cleanup routines."
                         media={[
                             "https://r2.e-z.host/4ed8b442-31c9-4738-a919-7ff8dee725df/vrv2o4g9.webp",
                             "https://r2.e-z.host/4ed8b442-31c9-4738-a919-7ff8dee725df/pu1z5f45.webp",
@@ -33,7 +33,7 @@ export default function Experience() {
                             "https://r2.e-z.host/4ed8b442-31c9-4738-a919-7ff8dee725df/x275xpf2.webp",
                         ]}
                         myRole="Founder"
-                        timeline="July 2024 - Present"
+                        timeline="April 2025 - Present"
                         delay={0.1}
                         gradient="bg-gradient-to-br"
                     />
